bd76a7dd0fe045eaa5724812b1c7a9deed9a1a64
ignaciocampillosserrano <ignacio.campillos.serrano@cieep.com>
2023-11-02 12:13:31 +0100

    Initial Commit

---
+++ XCODE-TableViewTests/XCODE_TableViewTests.swift
@@ -0,0 +1,33 @@
+//
+//  XCODE_TableViewTests.swift
+//  XCODE-TableViewTests
+//
+//  Created by Ignacio Campillos Serrano on 02/11/2023.
+//
+
+import XCTest
+@testable import XCODE_TableView
+
+class XCODE_TableViewTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
